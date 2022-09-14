4798742ae6887cf6623a0eae53f71d7d452cfe96
sauem <nguyendinhthang.go97@gmail.com>
2022-09-14 23:34:55 +0700

    a

--- wp-content/themes/frudbaz/template-parts/menu.php
+++ wp-content/themes/frudbaz/template-parts/menu.php
@@ -6,9 +6,9 @@ get_header();
 ?>
 <?php if (have_posts()): ?>
     <section class="slider bg-dark">
-        <div class="container">
+        <div class="container mt-5">
             <div class="row">
-                <div class="col-12">
+                <div class="col-md-8 offset-md-2">
                     <div class="main-slider">
                         <?php
                         query_posts([
