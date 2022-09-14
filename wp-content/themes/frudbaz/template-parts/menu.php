51441704355392ecaad45be2b75ab6d568816c0b
sauem <nguyendinhthang.go97@gmail.com>
2022-09-14 23:20:27 +0700

    update template

---
+++ wp-content/themes/frudbaz/template-parts/menu.php
@@ -0,0 +1,46 @@
+<?php
+/**
+ * Template Name: Home page
+ */
+get_header();
+?>
+<?php if (have_posts()): ?>
+    <section class="slider bg-dark">
+        <div class="container">
+            <div class="row">
+                <div class="col-12">
+                    <div class="main-slider">
+                        <?php
+                        query_posts([
+                            'post_type' => 'menu',
+                            'posts' => 99
+                        ]);
+                        while (have_posts()): the_post();
+                            ?>
+                            <div class="slide-item">
+                                <img src="<?= get_the_post_thumbnail_url() ?>" class="img-fluid"
+                                     alt="<?= get_post_meta(get_the_ID(), 'alt', true) ?>">
+                            </div>
+                        <?php endwhile;
+                        wp_reset_query(); ?>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <?php the_content() ?>
+<?php
+endif;
+get_footer();
+?>
+
+<script>
+
+    $('.main-slider').slick({
+        infinite: true,
+        slidesToShow: 1,
+        dots: true,
+        arrows: false
+    });
+
+</script>
